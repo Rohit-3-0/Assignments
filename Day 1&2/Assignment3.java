6236c6e93da0e419eb29919c593def08346368a4
Rohit-3-0 <r.karumanchi.1912@gmail.com>
2025-07-12 14:32:04 +0530

    .

--- Day 1&2/Assignment3.java
+++ Day 1&2/Assignment3.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 class Assignment3{
-    public static void main(String[]args){
+    public static string greetuser(String[]args){
+        System.out.println("Hello, " + name + "! Welcome to the MERN + DSA batch.");
+        greetuser(name);
+    }
+    public static void greetuser(String name) {
         Scanner scanner  = new Scanner (System.in);
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
         greetuser(name);
-    
-    }
-    public static void greetuser(String name) {
-        System.out.println("Hello, " + name + "! Welcome to the MERN + DSA batch.");
     }
 }
